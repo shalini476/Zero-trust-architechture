fb532309c4c9d103ed16c4808d3ffd9d4c7d55a5
Shalini P <shalinicsbs@gmail.com>
2026-06-19 12:39:39 +0530

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    # Bind to localhost:5000 in debug mode for development
-    app.run(host='127.0.0.1', port=5000, debug=True)
